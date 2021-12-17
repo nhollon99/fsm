e3b6bb0e5e6b79a5ea750cd7d6a6362c8aedf731
Nick <nrh3@williams.edu>
2021-12-17 16:44:22 -0500

    genus fixed

--- src/algorithms/genus.js
+++ src/algorithms/genus.js
@@ -1,6 +1,6 @@
 function genus() {
     nodeNumber = nodes.length
     edgeNumber = links.length
-    genus = edgeNumber - nodeNumber + 1
-    alert(`Genus of this graph: ${genus}`)
+    genusNum = edgeNumber - nodeNumber + 1
+    alert(`Genus of this graph: ${genusNum}`)
 }
